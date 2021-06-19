68ee65ee2c8094036272a9f5dc3b1c9363cd2db5
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-06-19 10:48:26 +0530

    adding componene

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: HomeComponent},
+  {path:"contactus", component:ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
