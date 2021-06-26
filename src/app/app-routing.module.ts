5c216d4f2bf7c12c5fc51d5e37243c4f6883b670
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-06-26 23:02:12 +0530

    routing test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
   {path:"home", component: HomeComponent},
-  {path:"contactus", component:ContactUsComponent}
+  {path:"contactus", component:ContactUsComponent},
+  {path:"test", component:TestComponent}
 ];
 
 @NgModule({
