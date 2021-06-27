dbbc80d16eaea837cc011f4fd05965bdcfca092b
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-06-27 10:51:56 +0530

    home modi

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TestService } from '../test.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private testService:TestService) { }
 
   ngOnInit(): void {
   }
+
+  test(){
+    // this.testService.Test().subscribe(res=>{
+    //   console.log(res)
+    console.log(this.testService.Test())
+    
+
+  }
+
 
 }
