47060b89ea8273c0eb2b5f0d159ed5423bb3ce6b
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-06-27 14:12:16 +0530

    url enc

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:true}))
+
 
 app.use(express.static(__dirname + '/dist/Test-heroku-Tukaram'));
+
+app.get('/test',(req,res) =>
+    res.send('response from backen')
+);
+
 app.get('/*', function(req,res) {
     
     console.log("port  "+ process.env.PORT)
@@ -19,9 +28,8 @@ app.get('/*', function(req,res) {
 //     res.sendFile('index.html', {root: 'dist/Test-heroku-Tukaram/'}),
 // );
 
-app.get('/test',(req,res) =>
-    res.send('response from backen')
-);
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,function(){
+    console.log("running the server on port env  "+process.env.PORT+ " port "+PORT )
+});
