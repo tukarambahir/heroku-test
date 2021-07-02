6be7040a940aa30cf2488aa21174991b1384eed2
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-07-02 16:00:32 +0530

    cors policy

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./db');
 const app = express();
-
+const cors = require('cors')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:true}))
 
+app.use(cors('*'))
 
 app.use(express.static(__dirname + '/dist/Test-heroku-Tukaram'));
 
