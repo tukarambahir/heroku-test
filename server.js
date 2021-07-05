a799dc617e015ffdd66c46d51da2fac478025258
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-07-05 13:15:42 +0530

    cors issue

--- server.js
+++ server.js
@@ -9,20 +9,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:true}))
 
 
-const forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
-  // Instruct the app
-  // to use the forceSSL
-  // middleware
-  app.use(forceSSL());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(cors('*'))
 // app.use(cors('https://ftw-ng-nodejs.herokuapp.com'))
