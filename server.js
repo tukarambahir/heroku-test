720c6d0e97040de0c53e7b162a9e0db8e77cb188
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-07-05 16:30:21 +0530

    domain

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended:true}))
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "herokuapp.com"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
